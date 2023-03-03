8a7fdc0da979c34c6d85a29bb71beeef22ac21f0
darwinalvarezp <darwinalvaezp.1@gmail.com>
2023-03-02 20:54:55 -0500

    creacion del proyectoapi rest

---
+++ src/test/java/com/procesos/concesionario/ConcesionarioApplicationTests.java
@@ -0,0 +1,13 @@
+package com.procesos.concesionario;
+
+import org.junit.jupiter.api.Test;
+import org.springframework.boot.test.context.SpringBootTest;
+
+@SpringBootTest
+class ConcesionarioApplicationTests {
+
+	@Test
+	void contextLoads() {
+	}
+
+}
